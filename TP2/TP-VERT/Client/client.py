8e60bab9308e3a244ff3d4d66d7bd68b6d879933
pH0xe <reig.julien@free.fr>
2021-10-12 17:14:39 +0200

    ajout fichier pour tp vert

---
+++ TP2/TP-VERT/Client/client.py
@@ -0,0 +1,27 @@
+import grpc
+import movie_pb2
+import movie_pb2_grpc
+
+
+def get_movie_by_id(stub, id):
+    movie = stub.GetMovieByID(id)
+    print(movie)
+
+def get_list_movies(stub):
+    allmovies = stub.GetListMovies(movie_pb2.Empty())
+    for movie in allmovies:
+        print("Movie called %s" % (movie.title))
+
+
+def run():
+    with grpc.insecure_channel('localhost:3001') as channel:
+        stub = movie_pb2_grpc.MovieStub(channel)
+        print("-------------- GetMovieByID --------------")
+        movieid = movie_pb2.MovieID(id="a8034f44-aee4-44cf-b32c-74cf452aaaae")
+        get_movie_by_id(stub, movieid)
+        print("-------------- GetListMovies --------------")
+        get_list_movies(stub)
+
+
+if __name__ == '__main__':
+    run()
