0e7b0de921d7d535fd2fb3f2c3c010425b0a06ad
pH0xe <reig.julien@free.fr>
2021-11-11 20:05:45 +0100

    tp2 vert user

--- TP2/TP-VERT/Movie/movie.py
+++ TP2/TP-VERT/Movie/movie.py
@@ -4,6 +4,12 @@ import movie_pb2_grpc
 import grpc
 from concurrent import futures
 
+BOOKING_PORT = 3004
+SHOWTIME_PORT = 3003
+USER_PORT = 3002
+MOVIE_PORT = 3001
+
+HOST = "localhost"
 
 class MovieServicer(movie_pb2_grpc.MovieServicer):
     def __init__(self):
@@ -11,6 +17,8 @@ class MovieServicer(movie_pb2_grpc.MovieServicer):
             self.db = json.load(jsf)["movies"]
 
     def GetMovieByID(self, request, context):
+        print("===== GetMovieById =====")
+
         for movie in self.db:
             if movie['id'] == request.id:
                 print("Movie found!")
@@ -19,10 +27,14 @@ class MovieServicer(movie_pb2_grpc.MovieServicer):
         return movie_pb2.MovieData(title="", rating="", director="", id="")
 
     def GetListMovies(self, request, context):
+        print("===== GetListMovie =====")
+
         for movie in self.db:
             yield movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'], id=movie['id'])
 
     def GetMovieByTitle(self, request, context):
+        print("===== GetMovieByTitle =====")
+
         for movie in self.db:
             if str(movie['title']) == request.title:
                 print('Movie found !')
@@ -30,6 +42,8 @@ class MovieServicer(movie_pb2_grpc.MovieServicer):
         return movie_pb2.MovieData()
 
     def CreateMovie(self, request, context):
+        print("===== CreateMovie =====")
+
         movie = self.findMovieById(request.id)
         if movie is not None:
             print('return here movie exist')
@@ -40,6 +54,8 @@ class MovieServicer(movie_pb2_grpc.MovieServicer):
         return movie_pb2.MovieData(title=request.title, rating=request.rating, director=request.director, id=request.id)
 
     def UpdateMovieRating(self, request, context):
+        print("===== UpdateMovieRating =====")
+
         id = request.id
         rating = request.rating
         movie = self.findMovieById(id)
@@ -50,6 +66,8 @@ class MovieServicer(movie_pb2_grpc.MovieServicer):
         return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'], id=movie['id'])
 
     def UpdateMovie(self, request, context):
+        print("===== UpdateMovie =====")
+
         movie = self.findMovieById(request.id)
         if movie is None:
             return movie_pb2.MovieData()
@@ -69,6 +87,8 @@ class MovieServicer(movie_pb2_grpc.MovieServicer):
         return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'], id=movie['id'])
 
     def DeleteMovie(self, request, context):
+        print("===== DeleteMovie =====")
+
         movie = self.findMovieById(request.id)
         if movie is None:
             return movie_pb2.MovieData()
@@ -76,6 +96,7 @@ class MovieServicer(movie_pb2_grpc.MovieServicer):
         return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'], id=movie['id'])
 
     def findMovieById(self, id):
+        print("===== findMovieById =====")
         for movie in self.db:
             if id == movie['id']:
                 return movie
@@ -85,7 +106,7 @@ class MovieServicer(movie_pb2_grpc.MovieServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     movie_pb2_grpc.add_MovieServicer_to_server(MovieServicer(), server)
-    server.add_insecure_port('[::]:3001')
+    server.add_insecure_port('[::]:' + str(MOVIE_PORT))
     server.start()
     server.wait_for_termination()
 
